37ce6402f2c41778567be07ca8761ca43115abb6
Rukhsar <rukhsar.man@gmail.com>
2016-12-12 10:03:52 +0000

    service provider added

---
+++ src/PackMeServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Rukhsar\PackMe;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackMeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
