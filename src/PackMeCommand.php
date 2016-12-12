add958cb6b589be83f86cb52aa1d80f60bde7da4
Rukhsar <rukhsar.man@gmail.com>
2016-12-12 11:49:45 +0000

    command variables and flow in process

--- src/PackMeCommand.php
+++ src/PackMeCommand.php
@@ -46,6 +46,25 @@ class PackMeCommand extends Command
 
         $bar->start();
 
+        // Command's common variables setup
+        $vendor = strtolower($this->argument('vendor'));
+        $name = strtolower($this->argument('name'));
+        $path = getcwd().'/packages/';
+        $fullPath = $path.$vendor.'/'.$name;
+
+        $cVendor = studly_case($vendor);
+        $cName = studly_case($name);
+
+        $requirement = '"psr-4": {
+            "' . $cVendor . '\\\\' . $cName . '\\\\": "packages/' . $vendor . '/' . $name . '/src",';
+
+        $appConfigLine = 'App\Providers\RouteServiceProvider::class,
+            ' . $cVendor . '\\' . $cName . '\\' . 'ServiceProvider::class,';
+
+        echo $appConfigLine;
+
+        $this->info('Creating package '.$vendor.'\\'.$name.'...');
+
 
     }
 }
